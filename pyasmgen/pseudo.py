0fa27adfdcdf6b8a925cec5f65037cbec79ae9da
albus-shore <dev@albus-shore.net>
2025-05-24 16:39:12 +0800

    Change: Refactor pseudos into 'Pseudo' class for more clear struct and IDE-friendly.

--- pyasmgen/pseudo.py
+++ pyasmgen/pseudo.py
@@ -13,72 +13,82 @@ def _address_check(address:str) -> bool:
     else:
         return False
 
-### ========================== Expose Instruction Functions ========================== ###
-def org(address:str) -> str:
-    '''The function is defined for working with 'ORG' pseudo.
-    Args:
-        address: A string indicates the strat address of following codes.
-    Returns:
-        A string indicate the valid 'ORG' pseudo.    
-    '''
-    if not _address_check(address):
-        warn('Abnormal ROM address.',UserWarning)
-    return f'ORG {address}'
+### ============================ Expose Pseudo Functions ============================ ###
+class Pseudo:
+    '''The class is defined to work with pseudos.'''
+
+    @staticmethod
+    def org(address:str) -> str:
+        '''The function is defined for working with 'ORG' pseudo.
+        Args:
+            address: A string indicates the strat address of following codes.
+        Returns:
+            A string indicate the valid 'ORG' pseudo.    
+        '''
+        if not _address_check(address):
+            warn('Abnormal ROM address.',UserWarning)
+        return f'ORG {address}'
     
-def end() -> str:
-    '''The function is defined for working with 'END' pseudo.
-    Returns:
-        A string indicate the valid 'END' pseudo.    
-    '''
-    return 'END'
+    @staticmethod
+    def end() -> str:
+        '''The function is defined for working with 'END' pseudo.
+        Returns:
+            A string indicate the valid 'END' pseudo.    
+        '''
+        return 'END'
 
-def equ(label:str,address:str) -> str:
-    '''The function is defined for working with 'EQU' pseudo.
-    Args:
-        label: A string indicates the label to define.
-        address: A string indicates the strat address of following codes.
-    Returns:
-        A string indicate the valid 'EQU' pseudo.    
-    '''
-    if not _address_check(address):
-        warn('Abnormal ROM address.',UserWarning)
-    return f'{label} EQU {address}'
+    @staticmethod
+    def equ(label:str,address:str) -> str:
+        '''The function is defined for working with 'EQU' pseudo.
+        Args:
+            label: A string indicates the label to define.
+            address: A string indicates the strat address of following codes.
+        Returns:
+            A string indicate the valid 'EQU' pseudo.    
+        '''
+        if not _address_check(address):
+            warn('Abnormal ROM address.',UserWarning)
+        return f'{label} EQU {address}'
 
-def db(*values) -> str:
-    '''The function is defined for working with 'DB' pseudo.
-    Args:
-        values: Some values indicates the values to define in ROM.
-    Returns:
-        A string indicate the valid 'DB' pseudo.    
-    '''
-    seperated_values = ','.join(str(value) for value in values)
-    return f'DB {seperated_values}'
+    @staticmethod
+    def db(*values) -> str:
+        '''The function is defined for working with 'DB' pseudo.
+        Args:
+            values: Some values indicates the values to define in ROM.
+        Returns:
+            A string indicate the valid 'DB' pseudo.    
+        '''
+        seperated_values = ','.join(str(value) for value in values)
+        return f'DB {seperated_values}'
 
-def dw(*values) -> str:
-    '''The function is defined for working with 'DW' pseudo.
-    Args:
-        values: Some values indicates the values to define in ROM.
-    Returns:
-        A string indicate the valid 'DW' pseudo.    
-    '''
-    seperated_values = ','.join(str(value) for value in values)
-    return f'DW {seperated_values}'
+    @staticmethod
+    def dw(*values) -> str:
+        '''The function is defined for working with 'DW' pseudo.
+        Args:
+            values: Some values indicates the values to define in ROM.
+        Returns:
+            A string indicate the valid 'DW' pseudo.    
+        '''
+        seperated_values = ','.join(str(value) for value in values)
+        return f'DW {seperated_values}'
 
-def ds(byte:str) -> str:
-    '''The function is defined for working with 'DS' pseudo.
-    Args:
-        byte: A string indicates the number of bytes to keep.
-    Returns:
-        A string indicate the valid 'DS' pseudo.    
-    '''
-    return f'DS {byte}'
+    @staticmethod
+    def ds(byte:str) -> str:
+        '''The function is defined for working with 'DS' pseudo.
+        Args:
+            byte: A string indicates the number of bytes to keep.
+        Returns:
+            A string indicate the valid 'DS' pseudo.    
+        '''
+        return f'DS {byte}'
 
-def bit(label:str,bit:str) -> str:
-    '''The function is defined for working with 'BIT' pseudo.
-    Args:
-        label: A string indicates the label to define.
-        bit: A string indicates the bit signed to the label.
-    Returns:
-        A string indicate the valid 'BIT' pseudo.    
-    '''
-    return f'{label} BIT {bit}'
+    @staticmethod
+    def bit(label:str,bit:str) -> str:
+        '''The function is defined for working with 'BIT' pseudo.
+        Args:
+            label: A string indicates the label to define.
+            bit: A string indicates the bit signed to the label.
+        Returns:
+            A string indicate the valid 'BIT' pseudo.    
+        '''
+        return f'{label} BIT {bit}'
